1709049fb17c48f1658d566e2b284321e9f36b0e
naxsyl79 <naxsyl79@gmail.com>
2024-06-08 14:08:01 +0000

    nyoba ngoding di idx

---
+++ wpu/DOM/latihan5/script.js
@@ -0,0 +1,10 @@
+const p3 = document.querySelector(".p3");
+const p2 = document.querySelector(".p2");
+
+function ubahWarnaP2() {
+  p3.style.backgroundColor = "salmon";
+}
+
+function ubahWarnaP3() {
+  p2.style.backgroundColor = "skyblue";
+}
