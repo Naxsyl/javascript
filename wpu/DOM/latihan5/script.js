409d7df654859d0f86a89a78673f52bcab9d113a
Naxsyl <naxsyl79@gmail.com>
2024-06-09 14:50:46 +0700

    lanjut dom 4 video lagi

--- wpu/DOM/latihan5/script.js
+++ wpu/DOM/latihan5/script.js
@@ -2,9 +2,20 @@ const p3 = document.querySelector(".p3");
 const p2 = document.querySelector(".p2");
 
 function ubahWarnaP2() {
-  p3.style.backgroundColor = "salmon";
+  p2.style.backgroundColor = "salmon";
 }
 
 function ubahWarnaP3() {
-  p2.style.backgroundColor = "skyblue";
+  p3.style.backgroundColor = "skyblue";
 }
+
+p2.onclick = ubahWarnaP2;
+
+const p4 = document.querySelector("section#b p");
+p4.addEventListener("click", function () {
+  const ul = document.querySelector("section#b ul");
+  const liBaru = document.createElement("li");
+  const teksLIBaru = document.createTextNode("Item Baru");
+  liBaru.appendChild(teksLIBaru);
+  ul.appendChild(liBaru);
+});
